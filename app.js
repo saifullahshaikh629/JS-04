3f8e6ff68eee353346fc8524029b12b7e277776a
saifullah <shaikhsaifullah629@gmail.com>
2022-11-14 03:16:37 -0800

    abc

---
+++ app.js
@@ -0,0 +1,214 @@
+alert("Welcome to Js Land ")
+// alert(" Happy Coding")
+
+// var book="Hello I can Run Js Through My Web Browsers Console"
+// alert(book);
+
+// var username="Saifullah"
+// var myname="Saifullah Shaikh"
+// var abc="Hello World"
+// alert(abc)
+
+// var av =("Jhoen doe")
+// alert(av)
+// var ac =("19 Years Old")
+// alert(ac)
+
+// var ab =("Certified Mob Application Development")
+// alert(ab)
+
+// var tx = ("Pizza" + "\n" + "Pizz" + "\n" + "Piz" + "\n" +"Pi" + "\n" + "P" )
+// alert(tx)
+
+
+// var a="My Email Address Is"
+// var email="shaikhsaifullah@gmail.com"
+// alert(a+email)
+
+// var dc="Yah! I Can Write Html Content THrough Java Script"
+// document.write(dc)
+
+// var age="I am 19 Years Older"
+// alert(age)
+
+// var visit="You Have Visited This Website 10 Times"
+// alert(visit)
+
+// var birthyear="15-June-2002"
+// alert(birthyear)
+
+// var nmbrstrng =("My Birth Year Is 2002" + "<br>" + "Data type Of My Declared Variable is Number")
+// document.write(nmbrstrng)
+
+
+// var a="Asad"
+// var b="Shirt"
+// var c="Quantity=5"
+
+// document.write(a + "<br>" + b + "<br>" + c  )
+
+// var d= "Asad Gromza Ordered 5 T-shirts"
+// document.write(d)
+
+
+// var a=3
+// var b=5
+// console.log(a+b)
+
+// var c=3
+// var d=5
+// console.log(c-d)
+
+// var e=3
+// var f=5
+// console.log(e*f)
+
+// var g=3
+// var h=5
+// console.log(h/g)
+
+
+// for(var i=1;i<=10;i++){
+//     document.write("4"+"x" + i + "="+ 4*i + "<br>")
+// }
+
+// var a=10
+// ++a
+// console.log(a)
+// a++
+// console.log(a)
+// --a
+// console.log(a)
+// a--
+// console.log(a)
+
+
+// var a=600
+// var b=5
+// console.log( "Total cost of buying 5 tikits are " +  a*b)
+
+// var a=2
+// var b=1
+// var c=--a
+// console.log(c)
+
+// var a=2
+// var b=1
+// var c=--a - --b
+// console.log(c)
+
+// var a=2
+// var b=1
+// var c=--a - --b + ++b
+// console.log(c)
+
+
+// var a=2
+// var b=1
+// var c=--a - --b + ++b + b--
+// console.log(c)
+
+// var name =prompt(" Hi! Enter Your User Name")
+
+
+// var a= prompt("Math" , "100")
+// console.log(a)
+// var b= prompt("Eng" , "100")
+// console.log(b)
+// var c= prompt("Urdu" , "100")
+// console.log(c)
+
+
+
+// var a=prompt("Enter Your City","Karachi")
+// if(a == "Karachi"){
+//     console.log("Welcome Karachi")
+// }
+
+// var a =prompt("Gender","Male"+"/Female")
+// if(a == "Male"){
+//     console.log("Hi Sir")
+// }if(a =="Female"){
+//     console.log("Hi Mam")
+// }
+
+
+// var a=prompt("Fuel")
+// if( a < 0.25){
+//     console.log("Pleasr Refill Your Car")
+// }
+
+
+// var a=4
+// if(++a ==5){
+//     alert("Given Condition For A Variabble Is True")
+// // }
+
+// var b = 82;0
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }else{
+//     alert("False")
+// }
+
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// var labourcost=20000
+// var materialcost=2000
+// var totalcost=labourcost + materialcost
+// if(totalcost == labourcost + materialcost ){
+//     alert("Cost is Equals to")
+// }
+
+
+
+// var a=prompt("Guess The Number")
+// if(a == 8){
+//     alert("Bingo")
+// }if(a == 9){
+//     alert("Close Enough To Answer")
+// }
+
+
+// var a=prompt("Temperature")
+// if(a == 40){
+//     console.log("It Is Hot Outside")
+// }if(a == 30){
+//     console.log("It Is Normsl")
+// }if(a == 20){
+//     console.log("Cool")
+// }if(a == 10){
+//     console.log("Too Cool")
+// }
+
+// var hour=13
+// if(hour< 18){
+// console.log("Good Day")
+// }else{
+//     console.log("Good Evening")
+// }
+
+// for(i = 0 ; i<=100 ; i++)
+// if(i % 2 ==0){
+//     console.log(i )
+//     // document.write( i + "<br>")
+// }
+
+// for(i = 0 ; i<=100 ; i++)
+// if(i % 2 !=0){
+//     console.log(i )
+//     document.write( i + "<br>")
+// }
